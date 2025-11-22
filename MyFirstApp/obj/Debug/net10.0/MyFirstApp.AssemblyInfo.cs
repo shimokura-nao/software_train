681e7d26bdb1089b0a5ffdff7120628d631779d7
shimokura <symtnr1208svv@gmail.com>
2025-11-22 23:49:54 +0900

    fix:バイナリファイルの更新とAssemblyInfoのバージョン修正

--- MyFirstApp/obj/Debug/net10.0/MyFirstApp.AssemblyInfo.cs
+++ MyFirstApp/obj/Debug/net10.0/MyFirstApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e653f622785b5e99853e3d5c86181d62faf97ee0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+346bfccf9ce2dab61fea84cdbde2b7ebe75ae973")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
